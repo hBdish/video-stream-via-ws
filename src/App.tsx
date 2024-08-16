dd52b189e2f6e5b0d103c74a8b4dd5c6a838842a
Maksim Doronkin <maksim.doronkin@neuro-city.ru>
2024-08-16 08:33:41 +0300

    v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,116 +1,31 @@
-
-import './App.css'
 import { useEffect, useRef, useState} from "react";
 
-const ws = new WebSocket("ws://localhost:8081");
-
-
+import VideoRecorder from "./VideoRecorder";
+import AudioRecorder from "./AudioRecorder";
+import './App.css'
 
 function App() {
-  const mediaSource = useRef(new MediaSource())
-  const sourceBuffer = useRef<SourceBuffer>();
-  const localVideoRef = useRef<HTMLVideoElement>(null);
-  const arrayOfBlobs = useRef<Blob[]>([]);
-
-
-
-  useEffect(() => {
-    ws.onopen = () => {
-      console.log("WebSocket connected");
+  const [recordOption, setRecordOption] = useState("video");
+  const toggleRecordOption = (type) => {
+    return () => {
+      setRecordOption(type);
     };
-
-    ws.onmessage = (event) => {
-
-      console.log("WebSocket message received", event.data);
-      if (event.data instanceof Blob) {
-        console.log(event.data)
-      }
-    };
-
-    void handleConnect()
-
-  }, []);
-
-
-
-  async function appendToSourceBuffer() {
-
-    if (!localVideoRef.current) return
-    if (!sourceBuffer.current) return
-
-    if (mediaSource.current.readyState === "open" && sourceBuffer && !sourceBuffer.current.updating) {
-
-    const neVal = arrayOfBlobs.current.shift()
-
-      if (!neVal) return
-
-      sourceBuffer.current.appendBuffer(await neVal.arrayBuffer());
-
-    }
-
-    // Limit total buffer size
-    if (localVideoRef.current.buffered.length && localVideoRef.current.buffered.end(0) - localVideoRef.current.buffered.start(0) > 2) {
-      sourceBuffer.current.remove(0, localVideoRef.current.buffered.end(0) - 1200);
-    }
-  }
-
-
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-expect-error
-  const getUserMedia = (constraints): Promise<MediaStream> => {
-    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      return navigator.mediaDevices.getUserMedia(constraints);
-    }
-
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-expect-error
-    const getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-
-    if (getUserMedia) {
-      return new Promise((resolve, reject) => getUserMedia.call(navigator, constraints, resolve, reject));
-    }
-
-    return Promise.reject(new Error('getUserMedia is not supported in this browser.'));
   };
-
-  const handleConnect = async () => {
-    try {
-      const stream = await getUserMedia({ video: true, audio: true });
-
-
-      const mediaRecorder = new MediaRecorder(stream, {
-        mimeType: 'video/webm' // Определяем тип видео
-      });
-
-      mediaRecorder.ondataavailable =  (event) => {
-
-        console.log(arrayOfBlobs.current)
-        if (!localVideoRef.current) return
-        arrayOfBlobs.current.push(event.data)
-        localVideoRef.current.src = URL.createObjectURL(event.data)
-      };
-
-      mediaSource.current.addEventListener("sourceopen", function() {
-        sourceBuffer.current = mediaSource.current.addSourceBuffer("video/webm; codecs=\"opus,vp8\"");
-        sourceBuffer.current.addEventListener("updateend", appendToSourceBuffer);
-
-      });
-
-      mediaRecorder.start(1000); // Отправка данных каждые 1000 мс
-
-    } catch (err) {
-      console.error('Error accessing media devices:', err);
-      alert(`Error accessing media devices: ${err}. Please check your browser permissions.`);
-    }
-
-
-  };
-
-
 
   return (
     <div>
-      <video  ref={localVideoRef} autoPlay controls></video>
+      <h1>React Media Recorder</h1>
+      <div className="button-flex">
+        <button onClick={toggleRecordOption("video")}>
+          Record Video
+        </button>
+        <button onClick={toggleRecordOption("audio")}>
+          Record Audio
+        </button>
+      </div>
+      <div>
+        {recordOption === "video" ? <VideoRecorder/> : <AudioRecorder/>}
+      </div>
     </div>
   )
 }
---
+++ src/VideoRecorder.tsx
@@ -0,0 +1,231 @@
+import {useState, useRef, useEffect} from "react";
+
+const ws = new WebSocket("ws://localhost:8081");
+ws.binaryType = 'arraybuffer'
+
+const mimeType = 'video/webm; codecs="opus,vp8"';
+
+
+const VideoRecorder = () => {
+  const [permission, setPermission] = useState(false);
+
+  const mediaRecorder = useRef(null);
+
+  const liveVideoFeed = useRef(null);
+
+  const wsVideoFeed = useRef<HTMLVideoElement>(null);
+
+  const wsVideoSource = useRef<MediaSource>();
+
+  const wsVideoSourceBuffer = useRef<SourceBuffer>();
+
+  const [recordingStatus, setRecordingStatus] = useState("inactive");
+
+  const [stream, setStream] = useState(null);
+
+  const [recordedVideo, setRecordedVideo] = useState(null);
+
+  const [videoChunks, setVideoChunks] = useState([]);
+
+
+  const arrayOfBlobs = useRef<Blob[]>([]);
+
+
+
+
+  useEffect(() => {
+    ws.onopen = () => {
+      console.log("WebSocket connected");
+    };
+
+    wsVideoSource.current = new MediaSource()
+
+    wsVideoFeed.current.src = URL.createObjectURL(wsVideoSource.current);
+
+    wsVideoSource.current.addEventListener("sourceopen", function() {
+      console.log("sourceopen")
+      wsVideoSourceBuffer.current = wsVideoSource.current.addSourceBuffer(mimeType)
+
+      wsVideoSourceBuffer.current.addEventListener("updateend", appendToSourceBuffer);
+    });
+
+  }, []);
+
+
+
+
+
+
+
+
+  async function appendToSourceBuffer() {
+    console.log("appendToSourceBuffer")
+    console.log(wsVideoSource.current.readyState)
+
+
+
+    if (wsVideoSource.current.readyState === "open" && wsVideoSourceBuffer.current && !wsVideoSourceBuffer.current.updating) {
+      const val = arrayOfBlobs.current.shift()
+
+      if (!val) return;
+
+      wsVideoSourceBuffer.current.appendBuffer(await val.arrayBuffer());
+    }
+
+    // Limit total buffer size
+    if (wsVideoFeed.current.buffered.length && wsVideoFeed.current.buffered.end(0) - wsVideoFeed.current.buffered.start(0) > 1200) {
+      wsVideoSourceBuffer.current.remove(0, wsVideoFeed.current.buffered.end(0) - 1200);
+    }
+  }
+
+  ws.onmessage =  (event) => {
+    // wsVideoChunks.current.push(event.data)
+    // const blob = new Blob(wsVideoChunks.current, {type: mimeType});
+
+    const blob = new Blob([event.data], {type: mimeType});
+    arrayOfBlobs.current.push(blob)
+    appendToSourceBuffer();
+
+    // wsVideoFeed.current.src = URL.createObjectURL(blob);
+    // const reader = new FileReader();
+    // reader.readAsDataURL(blob); // конвертирует Blob в base64 и вызывает onload
+
+    // reader.onload = function() {
+    //   // console.log(reader.result)
+    // };
+
+
+  };
+
+
+  const getCameraPermission = async () => {
+    setRecordedVideo(null);
+    //get video and audio permissions and then stream the result media stream to the videoSrc variable
+    if ("MediaRecorder" in window) {
+      try {
+        const videoConstraints = {
+          audio: false,
+          video: true,
+        };
+        const audioConstraints = { audio: true };
+
+        // create audio and video streams separately
+        const audioStream = await navigator.mediaDevices.getUserMedia(
+          audioConstraints
+        );
+        const videoStream = await navigator.mediaDevices.getUserMedia(
+          videoConstraints
+        );
+
+        setPermission(true);
+
+        //combine both audio and video streams
+
+        const combinedStream = new MediaStream([
+          ...videoStream.getVideoTracks(),
+          ...audioStream.getAudioTracks(),
+        ]);
+
+        setStream(combinedStream);
+
+        //set videostream to live feed player
+        liveVideoFeed.current.srcObject = videoStream;
+
+
+
+      } catch (err) {
+        alert(err.message);
+      }
+    } else {
+      alert("The MediaRecorder API is not supported in your browser.");
+    }
+  };
+
+  const startRecording = async () => {
+    setRecordingStatus("recording");
+
+    const media = new MediaRecorder(stream, { mimeType });
+
+    mediaRecorder.current = media;
+
+    mediaRecorder.current.start(200);
+
+    const localVideoChunks = [];
+
+    mediaRecorder.current.ondataavailable = (event) => {
+      // console.log(event.data)
+
+      if (typeof event.data === "undefined") return;
+      if (event.data.size === 0) return;
+      localVideoChunks.push(event.data);
+
+      ws.send(event.data)
+    };
+
+
+    setVideoChunks(localVideoChunks);
+  };
+
+  const stopRecording = () => {
+    setPermission(false);
+    setRecordingStatus("inactive");
+    mediaRecorder.current.stop();
+
+    mediaRecorder.current.onstop = () => {
+      const videoBlob = new Blob(videoChunks, { type: mimeType });
+      const videoUrl = URL.createObjectURL(videoBlob);
+
+      setRecordedVideo(videoUrl);
+
+      setVideoChunks([]);
+    };
+  };
+
+  return (
+    <div>
+      <h2>Video Recorder</h2>
+      <main>
+        <div className="video-controls">
+          {!permission ? (
+            <button onClick={getCameraPermission} type="button">
+              Get Camera
+            </button>
+          ) : null}
+          {permission && recordingStatus === "inactive" ? (
+            <button onClick={startRecording} type="button">
+              Start Recording
+            </button>
+          ) : null}
+          {recordingStatus === "recording" ? (
+            <button onClick={stopRecording} type="button">
+              Stop Recording
+            </button>
+          ) : null}
+        </div>
+      </main>
+
+      <div className="video-player">
+        {!recordedVideo ? (
+          <video ref={liveVideoFeed} autoPlay className="live-player"></video>
+        ) : null}
+        {recordedVideo ? (
+          <div className="recorded-player">
+            <video className="recorded" src={recordedVideo} controls></video>
+            <a download href={recordedVideo}>
+              Download Recording
+            </a>
+          </div>
+        ) : null}
+      </div>
+
+      <div className="video-player">
+
+          <video ref={wsVideoFeed} autoPlay className="live-player"></video>
+
+
+      </div>
+    </div>
+  );
+};
+
+export default VideoRecorder;
